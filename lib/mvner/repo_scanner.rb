662f981540b09f6a31805783c5b2a9db86b0b8e2
David Justice <david@devigned.com>
2012-11-30 23:03:12 -0500

    scanner knows about apk, apklib, and jar packaged projects within a scanned repo

--- lib/mvner/repo_scanner.rb
+++ lib/mvner/repo_scanner.rb
@@ -5,9 +5,21 @@ module Mvner
 
     def initialize(root = Dir.pwd)
       @root = root
-      @projects = Dir.glob(File.join(root, "**/pom.xml")).map{|loc|
+      @projects = Dir.glob(File.join(root, "**/pom.xml")).map { |loc|
         Project.new(loc)
       }
+    end
+
+    def apks
+      @projects.select { |proj| proj.packaging == "apk" }
+    end
+
+    def apklibs
+      @projects.select { |proj| proj.packaging == "apklib" }
+    end
+
+    def jars
+      @projects.select { |proj| proj.packaging == "jar" }
     end
   end
 end
--- spec/repo_scanner_spec.rb
+++ spec/repo_scanner_spec.rb
@@ -12,5 +12,17 @@ describe Mvner::RepoScanner do
       scanner.projects.select { |proj| proj.parent? }.count.should == 1
     end
 
+    it "should have 1 apk project" do
+      scanner.apks.count.should == 1
+    end
+
+    it "should have 2 apklib projects" do
+      scanner.apklibs.count.should == 2
+    end
+
+    it "should have 0 jar projects" do
+      scanner.jars.count.should == 0
+    end
+
   end
 end
